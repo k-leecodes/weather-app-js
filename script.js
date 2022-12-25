7c1a0399a12839a832aa82ffcb575c392c4ddc61
Kyra Lee <96844451+k-leecodes@users.noreply.github.com>
2022-12-24 23:34:51 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<p class="codepen" data-height="300" data-slug-hash="ExpVPpq" data-user="k-leecodes" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/k-leecodes/pen/ExpVPpq">
+  Weather App JS</a> by Kyra Lee (<a href="https://codepen.io/k-leecodes">@k-leecodes</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
