b95a4d05c8583f4ad5a8859321fc1a7835ce628e
Kyra Lee <96844451+k-leecodes@users.noreply.github.com>
2022-12-29 14:35:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,77 @@
-<p class="codepen" data-height="300" data-slug-hash="ExpVPpq" data-user="k-leecodes" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
-  <span>See the Pen <a href="https://codepen.io/k-leecodes/pen/ExpVPpq">
-  Weather App JS</a> by Kyra Lee (<a href="https://codepen.io/k-leecodes">@k-leecodes</a>)
-  on <a href="https://codepen.io">CodePen</a>.</span>
-</p>
-<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+    var exactTemp = (data.main.temp).toFixed(2)+'°C';
+    var maxTemp = (data.main.temp_max).toFixed(2)+'°C';
+    var minTemp = (data.main.temp_min).toFixed(2)+'°C';
+    var pres_bar = (data.main.pressure).toLocaleString() + 'mbar';
+    var windSpeed_kmh = ((data.wind.speed)/(1.609344)).toFixed(2)+'kmh';
+   
+    
+    //Imperial System Unit
+    var fTemp = Math.round((data.main.temp,5)*(9/5)+32)+'°F';
+    var exact_fTemp = ((data.main.temp*(9/5)+32)).toFixed(2)+'°F';
+    var max_fTemp = ((data.main.temp_max*(9/5)+32)).toFixed(2)+'°F';
+    var min_fTemp = ((data.main.temp_min*(9/5)+32)).toFixed(2)+'°F';
+    var pres_psig = (data.main.pressure/14.50377).toLocaleString() + 'psig';
+    var windSpeed_mph = (data.wind.speed)+'mph';
+    
+    
+    
+    var humid = data.main.humidity;
+    
+    
+    var desc = data.weather[0].description; 
+    var shortDesc = data.weather[0].main;
+    var icon = data.weather[0].icon;
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+   
+    var windDeg = data.wind.deg;
+    var weatherData;
+
+      }
+    }
+  });
+  */
+  //Back to Top Function;
+  /*
+  $('.back').click(function backtoTop(){
+  //Back to Top Button;
+    function backtoTop(){
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+      smoothScroll(event);
+     
+    };
+   
+  });
+  
+  
+  /*
+  var marginY = 0;
+  var destination = 0;
+  var scrollSpeed = 10;
+  var scroller = null;
+  
+  $('#scrollDown').click(function smoothScroll(scrollToID){
+    
+    destination = document.getElementById(scrollToID).offsetTop;
+    
+    scroller = setTimeout(function(){
+      smoothScroll(scrollToID);
+    }, 1); //function called every 1 mSec);
+    
+    marginY = marginY - scrollSpeed;
+    
+    if(marginY >= destination){
+      clearTimeout(scroller);
+    }
+    window.scroll (0, marginY);
+  });
+  return false;
+                 
+*/                  
+});//End of Tasks
+
+/*////Version 2
+1. Smooth scroll
+2. Speech bubble (Click to Convert Unit)
+*/
